856e3bc247c91642c73e24c61e5fbe7bb745bf55
Derek <derex1987@gmail.com>
2026-07-16 01:47:00 -0500

    memory management practice

--- go/comparch review/comparch.go
+++ go/comparch review/comparch.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// gonna review pointers from freshman year of college lol
+	x := 42
+	pointer1 := &x
+	fmt.Println(pointer1)
+	fmt.Println(x)
+	fmt.Println(*pointer1 + 1)
 }
