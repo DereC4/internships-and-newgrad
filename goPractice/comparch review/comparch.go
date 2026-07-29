cd1daf4d2949c3eed17448933057c4eaf3c9933d
Derek <derex1987@gmail.com>
2026-07-29 18:42:13 -0500

    feat: updated comparch review for funsies

--- goPractice/comparch review/comparch.go
+++ goPractice/comparch review/comparch.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
+// When passing structs to functions, Go copies the data
+type User struct {
+	Username string
+	Age      int
+	Active   bool
+}
+
+// alignment / order matters, just like in C!!!
+type BadStruct struct {
+	A bool  // 1 byte  (+ 7 bytes padding)
+	B int64 // 8 bytes
+	C bool  // 1 byte  (+ 7 bytes padding)
+}
+
+type GoodStrut struct {
+	B int64 // 8 bytes
+	A bool  // 1 byte
+	C bool  // 1 byte  (+ 6 bytes padding)
+}
+
 func main() {
 	// gonna review pointers from freshman year of college lol
 	x := 42
@@ -9,4 +29,11 @@ func main() {
 	fmt.Println(pointer1)
 	fmt.Println(x)
 	fmt.Println(*pointer1 + 1)
+
+	u1 := User{
+		Username: "alice_dev",
+		Age:      28,
+		Active:   true,
+	}
+	u1.Active = false
 }
