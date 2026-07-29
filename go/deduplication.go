08378d11ab7bb1e123ca8c224c23f300f5793b8f
Derek <derex1987@gmail.com>
2026-07-29 04:07:56 -0500

    feat: deduplicate job function

--- go/deduplication.go
+++ go/deduplication.go
@@ -1,5 +1,30 @@
 package main
 
-func deduplicateJobs(jobs []JobListing) []JobListing {
+import "strings"
 
+func deduplicateJobs(jobs []JobListing) []JobListing {
+	var uniqueJobs []JobListing
+	seenURLs := make(map[string]bool)
+	// seenComposites := make(map[string]bool)
+
+	for _, job := range jobs {
+		// grab just the link, no url params
+		cleanURL := cleanURL(job.Link)
+
+		if seenURLs[cleanURL] {
+			continue
+		}
+
+		seenURLs[cleanURL] = true
+		uniqueJobs = append(uniqueJobs, job)
+	}
+
+	return uniqueJobs
+}
+
+func cleanURL(raw string) string {
+	clean := strings.TrimSpace(raw)
+	clean = strings.Split(clean, "?")[0]
+	clean = strings.Split(clean, "#")[0]
+	return strings.ToLower(clean)
 }
