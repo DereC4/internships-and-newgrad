634a208aeca8d27e3af4e21418cf39437f53ccfd
Derek <derex1987@gmail.com>
2026-08-12 20:25:38 -0500

    feat: change badge wording

--- go/main.go
+++ go/main.go
@@ -147,8 +147,8 @@ func main() {
 	uniqueCompanyCount := len(companySet)
 
 	var badges strings.Builder
-	badges.WriteString(fmt.Sprintf("![Job Listings](https://img.shields.io/badge/Total_Scraped-%d-brightgreen?style=flat&logo=briefcase)\n", len(totalJobs)))
-	badges.WriteString(fmt.Sprintf("![Unique Listings](https://img.shields.io/badge/Total_Scraped-%d-brightgreen?style=flat&logo=briefcase)\n", len(uniqueJobs)))
+	badges.WriteString(fmt.Sprintf("![Job Listings](https://img.shields.io/badge/Aggregated_Jobs-%d-brightgreen?style=flat&logo=briefcase)\n", len(totalJobs)))
+	badges.WriteString(fmt.Sprintf("![Unique Listings](https://img.shields.io/badge/Unique_Listings-%d-brightgreen?style=flat&logo=briefcase)\n", len(uniqueJobs)))
 	badges.WriteString(fmt.Sprintf("![Companies](https://img.shields.io/badge/Companies-%d-blue?style=flat&logo=building)\n", uniqueCompanyCount))
 
 	for _, job := range uniqueJobs {
